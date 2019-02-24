dc44c7e660d473c3df2ab0d0e3c5f7c252e450ae
Juan Gaines <jdavid90.2@gmail.com>
2019-02-24 12:28:31 -0500

    feat: TODO list added to DetailsActivity
    Todo list added to the DetailsActivity in order to extarct the intent
    
    Resolves: NA
    See also: NA

--- app/src/main/java/com/mytechideas/explicitintentexample/DetailsActivity.java
+++ app/src/main/java/com/mytechideas/explicitintentexample/DetailsActivity.java
@@ -11,8 +11,13 @@ public class DetailsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
+
+        //TODO 16: save and get the incoming intent with the getIntent method
         Intent intent = getIntent();
 
+        //TODO 17: Check if the intent is different from null
+            //TODO 18: If the intent is valid, you can extract the data
+            //TODO 19: Log the result of the incoming data
         if(intent!=null){
             String textFromMainActivity=intent.getStringExtra("key_contact");
             Log.d(DetailsActivity.class.getSimpleName(),"The data from intent is:" +textFromMainActivity);
